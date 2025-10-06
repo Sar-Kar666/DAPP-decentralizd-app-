e22cf266240e3622d6a6223f525296cf3a7c48f7
Diptanu Sarkar <sarkar@Diptanus-MacBook-Air.local>
2025-10-06 11:40:07 +0530

    created a dapp in solana

---
+++ src/Airdrop.jsx
@@ -0,0 +1,17 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+
+export function Airdrop(){
+
+    const walet=useWallet();
+    const {connection}=useConnection();
+    
+
+    async function sendAirdropToUser(){
+        const amount=document.getElementById("publickey").value;
+       await connection.requestAirdrop(walet.publicKey,amount*1000000000)
+    }
+    return<div>
+<input id="publickey" placeholder="amount"/>
+<button onClick={sendAirdropToUser}>send Airdrop</button>
+    </div>
+}   
